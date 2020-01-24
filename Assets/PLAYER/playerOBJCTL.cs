40f861b4100f17dbfbafeb61079a3bab677ff4a7
kaminoda <misen6m@yahoo.co.jp>
2020-01-24 09:03:34 +0900

    1/24
    弾幕とスクロール作成

---
+++ Assets/PLAYER/playerOBJCTL.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerOBJCTL : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject tama2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("Tam");
+    }
+    IEnumerator Tam()
+    {
+        while (true)
+        {
+            //弾を生成
+            Instantiate(tama2, transform.position, Quaternion.identity);
+            tama2.transform.position = transform.position;
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/PLAYER/tamaCTL.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tamaCTL : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = transform.up.normalized * 15;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //画面外に出たら削除
+        if (!GetComponent<Renderer>().isVisible)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gameObject.CompareTag("enemy"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
